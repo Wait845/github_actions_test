aaf249385b6bce03bfb21ce43bf733d3eaad0998
vogel <vogell845@gmail.com>
2022-03-27 16:30:33 +0800

    action 4

---
+++ pt1.py
@@ -0,0 +1,13 @@
+import pytest
+
+def test_t1():
+    print("This is t1")
+    assert 1 == 1
+
+def test_t2():
+    print("This is t2")
+    assert 2 == 1
+
+def test_t3():
+    print("This is t3")
+    assert 3==3
